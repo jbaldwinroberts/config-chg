b25bea854364ceb8b07ced128d067bc932bcc88c
jroberts <jroberts@luno.com>
2023-02-07 09:30:03 +0000

    Implement loading a single json file

--- config/config.go
+++ config/config.go
@@ -1,1 +1,28 @@
 package config
+
+import (
+	"encoding/json"
+	"io/fs"
+)
+
+type Config struct {
+	fileSystem fs.FS
+}
+
+func New(fs fs.FS) Config {
+	return Config{fs}
+}
+
+func (c *Config) loadJson(filename string) (map[string]any, error) {
+	file, err := fs.ReadFile(c.fileSystem, filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var data map[string]any
+	if err := json.Unmarshal(file, &data); err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
--- config/config_test.go
+++ config/config_test.go
@@ -1,1 +1,114 @@
 package config
+
+import (
+	"github.com/google/go-cmp/cmp"
+	"testing"
+	"testing/fstest"
+)
+
+const (
+	config = `{
+	 "environment": "production",
+	 "database": {
+	   "host": "mysql",
+	   "port": 3306,
+	   "username": "divido",
+	   "password": "divido"
+	 },
+	 "cache": {
+	   "redis": {
+	     "host": "redis",
+	     "port": 6379
+	   }
+	 }
+	}`
+
+	configLocal = `{
+  "environment": "development",
+  "database": {
+    "host": "127.0.0.1",
+    "port": 3306,
+    "username": "divido",
+    "password": "divido"
+  },
+  "cache": {
+    "redis": {
+      "host": "127.0.0.1",
+      "port": 6379
+    }
+  }
+}`
+	configInvalid = `This is not a valid JSON file`
+)
+
+func TestLoadJson(t *testing.T) {
+	t.Run("with a missing file", func(t *testing.T) {
+		fs := fstest.MapFS{}
+
+		c := New(fs)
+		_, err := c.loadJson("missing.json")
+		assertError(t, err)
+	})
+
+	t.Run("with an invalid json file", func(t *testing.T) {
+		fs := fstest.MapFS{
+			"configInvalid.json": {Data: []byte(configInvalid)},
+		}
+
+		c := New(fs)
+		_, err := c.loadJson("configInvalid.json")
+		assertError(t, err)
+	})
+
+	t.Run("with a valid json file", func(t *testing.T) {
+		fs := fstest.MapFS{
+			"config.json": {Data: []byte(config)},
+		}
+
+		c := New(fs)
+		got, err := c.loadJson("config.json")
+		assertNilError(t, err)
+
+		want := map[string]any{
+			"environment": "production",
+			"database": map[string]any{
+				"host":     "mysql",
+				"port":     float64(3306),
+				"username": "divido",
+				"password": "divido",
+			},
+			"cache": map[string]any{
+				"redis": map[string]any{
+					"host": "redis",
+					"port": float64(6379),
+				},
+			},
+		}
+
+		assertMap(t, got, want)
+	})
+}
+
+func assertError(t *testing.T, err error) {
+	t.Helper()
+
+	if err == nil {
+		t.Fatalf("did not get expected error")
+	}
+}
+
+func assertNilError(t *testing.T, err error) {
+	t.Helper()
+
+	if err != nil {
+		t.Fatalf("got an unexpected error: %v", err)
+	}
+}
+
+func assertMap(t *testing.T, got, want map[string]any) {
+	t.Helper()
+
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("assert mismatch (-want +got):\n%s", diff)
+	}
+}
